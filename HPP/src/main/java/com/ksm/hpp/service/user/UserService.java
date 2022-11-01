dca65f87cebf1f3173bf3920bde759c6284a26e2
yhoo0094 <69442322+yhoo0094@users.noreply.github.com>
2022-11-01 18:08:08 +0900

    DB커밋

--- HPP/src/main/java/com/ksm/hpp/service/user/UserService.java
+++ HPP/src/main/java/com/ksm/hpp/service/user/UserService.java
@@ -3,18 +3,23 @@ package com.ksm.hpp.service.user;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ksm.hpp.service.com.BaseService;
 
 @Service("UserService")
-public class UserService extends BaseService {
+public class UserService {
+	
+	@Autowired
+	SqlSession sqlSession; //SqlSession 빈 DI	
 	
 	public Map<String, Object> insertUser(StringBuilder logStr, Map<String, Object> inData) throws Exception
 	{
 		Map<String, Object> result = new HashMap<String, Object>();
 		
-		this.mtDao.queryForInsert("mapper.user.UserMapper.insertUser", inData);
+		sqlSession.insert("mapper.user.UserMapper.insertUser", inData);
 		
 		return result;
 	}	
