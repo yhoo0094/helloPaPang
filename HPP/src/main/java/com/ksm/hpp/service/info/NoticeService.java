8660eef2f40a8acb8fe9fb14edc9efa4ae074a75
yhoo0094 <69442322+yhoo0094@users.noreply.github.com>
2022-12-07 09:03:46 +0900

    파일첨부 서버단 구현 중

--- HPP/src/main/java/com/ksm/hpp/service/info/NoticeService.java
+++ HPP/src/main/java/com/ksm/hpp/service/info/NoticeService.java
@@ -1,5 +1,6 @@
 package com.ksm.hpp.service.info;
 
+import java.io.File;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
@@ -29,11 +30,34 @@ public class NoticeService {
 	 */	
 	public Map<String, Object> saveNotice(StringBuilder logStr, Map<String, Object> inData, List<MultipartFile> fileList) throws Exception
 	{
+		Map<String, Object> result = new HashMap<String, Object>();
+		
 		LocalDate date = LocalDate.now();
 		DateTimeFormatter dtfDate = DateTimeFormatter.ofPattern("yyyyMMdd");
+		String nowDate = date.format(dtfDate);
+		
+		LocalDate time = LocalDate.now();
+		DateTimeFormatter dtfTime = DateTimeFormatter.ofPattern("HHmmss");
+		String nowTime = time.format(dtfTime);
+		
+		do {
+			if(fileList == null) {
+				result.put(Constant.RESULT, Constant.RESULT_FAILURE);
+				result.put(Constant.OUT_RESULT_MSG, "첨부파일이 존재하지 않습니다.");	
+				break;
+			}
+		} while(false);
+		
+		String filePath = "";	//첨부파일 저장 경로
+		
+		File dir = new File(filePath);
+		if(!dir.isDirectory()) {	//해당 경로가 디렉토리인지 확인
+			if(!dir.exists()) {		//해당 경로 디렉토리가 있는지 확인
+				dir.mkdir();
+			}
+		}
 		
 		
-		Map<String, Object> result = new HashMap<String, Object>();
 		
 		/*
 		List<Object> list = sqlSession.selectList("mapper.user.UserMapper.selectUser", inData);
